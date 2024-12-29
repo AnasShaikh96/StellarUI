3ee8f2eccf03424e6cc145ac4fd55400b47f7e38
Anas Shaikh <shaikhanas246@gmail.com>
2024-12-29 19:17:27 +0530

    add homepage navbar

--- src/app/ui/navbar.tsx
+++ src/app/ui/navbar.tsx
@@ -1,117 +1,39 @@
-import Image from "next/image";
+"use client";
+// import Image from "next/image";
+import { Bars3Icon } from "@heroicons/react/24/outline";
 
 export default function Navbar() {
   return (
-    <nav className="bg-gray-800">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="relative flex h-16 items-center justify-between">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            <button
-              type="button"
-              className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-              aria-controls="mobile-menu"
-              aria-expanded="false"
-            >
-              <span className="absolute -inset-0.5"></span>
-              <span className="sr-only">Open main menu</span>
+    <nav className="bg-slate-100">
+      <div className="mx-auto max-w-7xl  px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 justify-between items-center gap-x-4 lg:px-8 sm:px-6 ">
+          <div className="absolute left-0 inset-y-0 flex items-center">
+            <button className="py-3 sm:hidden">
+              <Bars3Icon className="block size-6" />
             </button>
           </div>
-          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex shrink-0 items-center">
-              <Image
-                src={
-                  "https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500"
-                }
-                width={"100"}
-                height={"100"}
-                className="h-8 w-auto"
-                alt="Your Company"
-              />
-            </div>
-            <div className="hidden sm:ml-6 sm:block">
-              <div className="flex space-x-4">
-                <a
-                  href="#"
-                  className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
-                  aria-current="page"
-                >
-                  Dashboard
-                </a>
-                <a
-                  href="#"
-                  className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                >
-                  Team
-                </a>
-                <a
-                  href="#"
-                  className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                >
-                  Projects
-                </a>
-                <a
-                  href="#"
-                  className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                >
-                  Calendar
-                </a>
-              </div>
+          <div className="flex flex-1 justify-center sm:items-center sm:justify-start">
+            <span>Brand Img</span>
+            <div className="ml-6 hidden sm:block">
+              <ul className="flex  my-2">
+                <li className="px-3 py-2  rounded-sm text-sm font-medium bg-slate-400">
+                  <a href="#">Documentation</a>
+                </li>
+                <li className="px-3 py-2  rounded-sm text-sm font-medium">
+                  <a href="#">Component</a>
+                </li>
+                <li className="px-3 py-2  rounded-sm text-sm font-medium">
+                  <a href="#">Templates</a>
+                </li>
+              </ul>
             </div>
           </div>
-          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <button
-              type="button"
-              className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-            >
-              <span className="absolute -inset-1.5"></span>
-              <span className="sr-only">View notifications</span>
+
+          <div className="absolute right-0 inset-y-0 flex items-center">
+            <button className="bg-violet-600 px-3 py-2 text-sm text-white rounded-md font-medium">
+              Contact
             </button>
-
-            <div className="relative ml-3">
-              <div>
-                <button
-                  type="button"
-                  className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                  id="user-menu-button"
-                  aria-expanded="false"
-                  aria-haspopup="true"
-                >
-                  <span className="absolute -inset-1.5"></span>
-                  <span className="sr-only">Open user menu</span>
-                </button>
-              </div>
-            </div>
           </div>
-        </div>
-      </div>
-
-      <div className="sm:hidden" id="mobile-menu">
-        <div className="space-y-1 px-2 pb-3 pt-2">
-          <a
-            href="#"
-            className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
-            aria-current="page"
-          >
-            Dashboard
-          </a>
-          <a
-            href="#"
-            className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-          >
-            Team
-          </a>
-          <a
-            href="#"
-            className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-          >
-            Projects
-          </a>
-          <a
-            href="#"
-            className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-          >
-            Calendar
-          </a>
         </div>
       </div>
     </nav>
